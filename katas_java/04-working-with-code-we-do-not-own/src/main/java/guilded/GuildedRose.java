1b4a6975cb6c87dd5007b04f9621aae6154cc00c
Antonio de la Torre <antonio@codesai.com>
2019-04-09 10:36:48 +0200

    Store support a list of items

--- katas_java/04-working-with-code-we-do-not-own/src/main/java/guilded/GuildedRose.java
+++ katas_java/04-working-with-code-we-do-not-own/src/main/java/guilded/GuildedRose.java
@@ -2,16 +2,28 @@ package guilded;
 
 import items.Item;
 
+import java.util.ArrayList;
+
 public class GuildedRose {
 
-    private final Item item;
+    private Item item;
+    private ArrayList<Item> items;
 
     public GuildedRose(Item item) {
-        this.item = item;
+        ArrayList<Item> items = new ArrayList<>();
+        items.add(item);
+        this.items = items;
+    }
+
+    public GuildedRose(ArrayList<Item> items) {
+
+        this.items = items;
     }
 
     public void updatesInventory() {
-        item.sellIn -= 1;
-        item.quality -= 1;
+        items.stream().forEach(item -> {
+            item.sellIn -= 1;
+            item.quality -= 1;
+        });
     }
 }
--- katas_java/04-working-with-code-we-do-not-own/src/test/java/GildedRoseTest.java
+++ katas_java/04-working-with-code-we-do-not-own/src/test/java/GildedRoseTest.java
@@ -2,6 +2,8 @@ import guilded.GuildedRose;
 import items.Item;
 import org.junit.Test;
 
+import java.util.ArrayList;
+
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 
@@ -19,5 +21,34 @@ public class GildedRoseTest {
         assertThat(item.sellIn, is (0));
     }
 
+    @Test
+    public void decrease_all_items_in_store() {
+        Item apple = new Item("apple", 1, 1);
+        Item orange = new Item("orange", 5, 20);
+
+
+        ArrayList<Item> items = new ArrayList<>();
+        items.add(apple);
+        items.add(orange);
+        GuildedRose store = new GuildedRose(items);
+
+        store.updatesInventory();
+
+        assertThat(apple.name, is("apple"));
+        assertThat(apple.quality, is(0));
+        assertThat(apple.sellIn, is (0));
+
+        assertThat(orange.name, is("orange"));
+        assertThat(orange.quality, is(19));
+        assertThat(orange.sellIn, is (4));
+    }
+
 
 }
+
+
+// 2 items
+
+
+// date has passed, quality degrades twice as fast
+//
