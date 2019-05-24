604596eca0b07185b5c94a44a9dfe5c6bcda18cf
Antonio de la Torre <antondelatorre@gmail.com>
2019-05-24 15:45:53 +0200

    Pass two values in params

--- katas_java/06-refactoring-to-hexagonal-architecture/src/main/java/birthdaygreetings/FileEmployeeRepository.java
+++ katas_java/06-refactoring-to-hexagonal-architecture/src/main/java/birthdaygreetings/FileEmployeeRepository.java
@@ -5,6 +5,7 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.text.ParseException;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 public class FileEmployeeRepository implements EmployeeRepository {
@@ -46,10 +47,12 @@ public class FileEmployeeRepository implements EmployeeRepository {
     }
 
     private Employee createEmployeeFromData(String[] employeeData) throws ParseException {
+        String[] dateSplitted = employeeData[2].split("/");
+        Date birthDate = new Date(Integer.parseInt(dateSplitted[0]), Integer.parseInt(dateSplitted[1]) - 1, Integer.parseInt(dateSplitted[2]));
 
 
         return new Employee(employeeData[1], employeeData[0],
-                employeeData[2], employeeData[3], null);
+                employeeData[2], employeeData[3], birthDate);
     }
 
     private String[] parseEmployeeData(String str) {
