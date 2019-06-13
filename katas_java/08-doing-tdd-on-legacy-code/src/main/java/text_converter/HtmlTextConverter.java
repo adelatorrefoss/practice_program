9b9db416994560464eee90c71052eb01d0a18688
Gonzalo Pascual <gonzalo.pascual@lifullconnect.com>
2019-06-13 10:54:52 +0200

    Add single and double quotes char replacement

--- katas_java/08-doing-tdd-on-legacy-code/src/main/java/text_converter/HtmlTextConverter.java
+++ katas_java/08-doing-tdd-on-legacy-code/src/main/java/text_converter/HtmlTextConverter.java
@@ -33,6 +33,8 @@ public class HtmlTextConverter
 		output = output.replace("&", "&amp;");
 		output = output.replace("<", "&lt;");
 		output = output.replace(">", "&gt;");
+		output = output.replace("\"", "&quot;");
+		output = output.replace("'", "&quot;");
 		return output;
 	}
 
--- katas_java/08-doing-tdd-on-legacy-code/src/test/java/text_converter/HtmlTextConverterTest.java
+++ katas_java/08-doing-tdd-on-legacy-code/src/test/java/text_converter/HtmlTextConverterTest.java
@@ -8,12 +8,12 @@ import static org.hamcrest.Matchers.equalTo;
 public class HtmlTextConverterTest {
 
     @Test
-    public void replace_ampersand_gt_and_lt() {
+    public void replace_ampersand_gt_lt_single_and_double_quotes() {
         HtmlTextConverter converter = new HtmlTextConverter("");
-        String line = "<first>&and</last>";
+        String line = "<first>&and\"double\"'single'</last>";
         String result = converter.replaceSpecialChars(line);
 
-        String expected = "&lt;first&gt;&amp;and&lt;/last&gt;";
+        String expected = "&lt;first&gt;&amp;and&quot;double&quot;&quot;single&quot;&lt;/last&gt;";
         assertThat(expected, equalTo(result));
     }
 }
