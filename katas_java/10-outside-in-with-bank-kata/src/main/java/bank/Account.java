2761d9ef31f7e3fecf19b2105fe2ff8e8b93cf36
Antonio de la Torre <antonio@codesai.com>
2020-01-14 00:43:12 +0100

    Make a withdraw

--- katas_java/10-outside-in-with-bank-kata/src/main/java/bank/Account.java
+++ katas_java/10-outside-in-with-bank-kata/src/main/java/bank/Account.java
@@ -22,5 +22,6 @@ public class Account {
     }
 
     public void withdraw(int amount) {
+        deposit(-amount);
     }
 }
--- katas_java/10-outside-in-with-bank-kata/src/test/java/bank/tests/unit/AccountTest.java
+++ katas_java/10-outside-in-with-bank-kata/src/test/java/bank/tests/unit/AccountTest.java
@@ -10,6 +10,7 @@ import java.util.Date;
 import static org.mockito.Mockito.*;
 
 public class AccountTest {
+
     @Test
     public void make_a_deposit() throws ParseException {
         Date today = new SimpleDateFormat("dd-MM-yyyy").parse("10-01-2012");
@@ -23,6 +24,23 @@ public class AccountTest {
         account.deposit(1000);
 
         Transaction transaction = new Transaction(today, 1000);
+
+        verify(transactionRepository).save(transaction);
+    }
+
+    @Test
+    public void make_a_withdraw() throws ParseException {
+        Date today = new SimpleDateFormat("dd-MM-yyyy").parse("13-01-2012");
+
+        Printer printer = mock(Printer.class);
+        Calendar calendar = mock(Calendar.class);
+        when(calendar.today()).thenReturn(today);
+        TransactionRepository transactionRepository = mock(TransactionRepository.class);
+
+        Account account = new Account(calendar, printer, transactionRepository);
+        account.withdraw(500);
+
+        Transaction transaction = new Transaction(today, -500);
 
         verify(transactionRepository).save(transaction);
     }
