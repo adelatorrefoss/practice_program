a46a7074d47bf9bc9539d5af5c1190a2d09bd5a8
Antonio de la Torre <antonio@codesai.com>
2020-01-14 00:46:02 +0100

    Refactor tests

--- katas_java/10-outside-in-with-bank-kata/src/test/java/bank/tests/unit/AccountTest.java
+++ katas_java/10-outside-in-with-bank-kata/src/test/java/bank/tests/unit/AccountTest.java
@@ -1,6 +1,7 @@
 package bank.tests.unit;
 
 import bank.*;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.text.ParseException;
@@ -11,37 +12,36 @@ import static org.mockito.Mockito.*;
 
 public class AccountTest {
 
+    private Account account;
+    private Calendar calendar;
+    private TransactionRepository transactionRepository;
+
+    @Before
+    public void setUp() {
+        Printer printer = mock(Printer.class);
+        calendar = mock(Calendar.class);
+        transactionRepository = mock(TransactionRepository.class);
+
+        account = new Account(calendar, printer, transactionRepository);
+    }
+
     @Test
     public void make_a_deposit() throws ParseException {
         Date today = new SimpleDateFormat("dd-MM-yyyy").parse("10-01-2012");
-
-        Printer printer = mock(Printer.class);
-        Calendar calendar = mock(Calendar.class);
         when(calendar.today()).thenReturn(today);
-        TransactionRepository transactionRepository = mock(TransactionRepository.class);
-
-        Account account = new Account(calendar, printer, transactionRepository);
         account.deposit(1000);
 
         Transaction transaction = new Transaction(today, 1000);
-
         verify(transactionRepository).save(transaction);
     }
 
     @Test
     public void make_a_withdraw() throws ParseException {
         Date today = new SimpleDateFormat("dd-MM-yyyy").parse("13-01-2012");
-
-        Printer printer = mock(Printer.class);
-        Calendar calendar = mock(Calendar.class);
         when(calendar.today()).thenReturn(today);
-        TransactionRepository transactionRepository = mock(TransactionRepository.class);
-
-        Account account = new Account(calendar, printer, transactionRepository);
         account.withdraw(500);
 
         Transaction transaction = new Transaction(today, -500);
-
         verify(transactionRepository).save(transaction);
     }
 }
