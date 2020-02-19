4a773ddee8e7ece8199c6dd8844932af8ceb0024
Antonio de la Torre <antonio@codesai.com>
2020-02-19 11:57:47 +0100

    should_send_a_notification_when_there_are_unusual_expenses

--- katas_java/11-outside-in-with-unusual-spending-alert-service/src/test/java/alert_service/tests/unit/UnusualExpensesNotifierServiceTest.java
+++ katas_java/11-outside-in-with-unusual-spending-alert-service/src/test/java/alert_service/tests/unit/UnusualExpensesNotifierServiceTest.java
@@ -3,6 +3,7 @@ package alert_service.tests.unit;
 import alert_service.*;
 import org.junit.Test;
 
+import java.util.Arrays;
 import java.util.Collections;
 
 import static org.mockito.Mockito.*;
@@ -25,5 +26,24 @@ public class UnusualExpensesNotifierServiceTest {
         unusualSpendingsNotifierService.alertOfUnusualSpendings(userId);
 
         verify(notifier, never()).send(any());
+    }
+
+    @Test
+    public void should_send_a_notification_when_there_are_unusual_expenses() {
+        int userId = USER_ID;
+        UnusualSpendingsDetector unusualSpendingDetector = mock(MonthlyUnusualSpendingsDetector.class);
+        Notifier notifier = mock(TextMessageNotifier.class);
+        double anyAmount = 1.0;
+        String anyCategory = "AnyCategory";
+        UnusualExpenses unusualExpenses = new UnusualExpenses(userId,
+                Arrays.asList(new UnusualSpending(anyCategory, anyAmount)
+                ));
+        when(unusualSpendingDetector.execute(userId)).thenReturn(unusualExpenses);
+
+        UnusualSpendingsNotifierService unusualSpendingsNotifierService =
+                new UnusualSpendingsNotifierService(unusualSpendingDetector, notifier);
+        unusualSpendingsNotifierService.alertOfUnusualSpendings(userId);
+
+        verify(notifier, times(1)).send(unusualExpenses);
     }
 }
